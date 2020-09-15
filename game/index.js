59c056f8c09606fdb967f350301a64f699bd4705
Torre <tostaylo1@gmail.com>
2020-09-14 21:39:18 -0500

    refactoring for SRP

--- game/index.js
+++ game/index.js
@@ -11,38 +11,47 @@ function timeout(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function renderWord() {
-	const splitWord = secretWord.split('');
-	const letterMap = splitWord.map((letter) => [letter, guessed.includes(letter)]);
+function renderWord(letterMap) {
 	const wordDiv = document.getElementById('secretWord');
 	const letterSpans = letterMap.map(([letter, isGuessed]) => `<span>${isGuessed ? letter : '_'}</span>`).join('');
 	wordDiv.innerHTML = letterSpans;
+}
+
+function calculateGuess(word) {
+	const letterMap = word.map((letter) => [letter, guessed.includes(letter)]);
+
 	// eslint-disable-next-line no-unused-vars
-	return letterMap.every(([_letter, isGuessed]) => isGuessed);
+	return [letterMap.every(([_letter, isGuessed]) => isGuessed), letterMap];
 }
 
 async function handleGuess() {
 	const guessInput = document.getElementById('guess');
-	if (counter < 6) {
-		if (!guessed.includes(guessInput.value) && secretWord.includes(guessInput.value)) {
-			guessed.push(guessInput.value);
-			guessInput.value = '';
-			if (renderWord()) {
-				await timeout(500);
-				if (confirm('You Won! Play Again?')) {
-					window.location.reload();
-				}
+
+	// This condition handles repeat guesses.
+	if (!guessed.includes(guessInput.value) && secretWord.includes(guessInput.value)) {
+		guessed.push(guessInput.value);
+		guessInput.value = '';
+
+		const [isWordGuessed, letterMap] = calculateGuess(secretWord);
+		renderWord(letterMap);
+
+		if (isWordGuessed) {
+			await timeout(500);
+
+			if (confirm('You Won! Play Again?')) {
+				window.location.reload();
 			}
-		} else {
-			counter += 1;
-			document.getElementById('strikes').innerText = (6 - counter).toString();
-			guessInput.value = '';
-			if (counter >= 6) {
-				await timeout(500);
-				// eslint-disable-next-line no-lonely-if
-				if (confirm('You Lost. Play Again?')) {
-					window.location.reload();
-				}
+		}
+	} else {
+		counter += 1;
+		document.getElementById('strikes').innerText = (6 - counter).toString();
+		guessInput.value = '';
+
+		if (counter >= 6) {
+			await timeout(500);
+
+			if (confirm('You Lost. Play Again?')) {
+				window.location.reload();
 			}
 		}
 	}
@@ -63,10 +72,11 @@ function start() {
 		'invisible',
 	];
 	const submitButton = document.getElementById('submit');
-	secretWord = words[Math.floor(Math.random() * words.length + 0)];
-	console.log(secretWord);
-	renderWord();
+	secretWord = words[Math.floor(Math.random() * words.length + 0)].split('');
 
+	// eslint-disable-next-line no-unused-vars
+	const [_isWordGuessed, letterMap] = calculateGuess(secretWord);
+	renderWord(letterMap);
 	submitButton.addEventListener('click', () => handleGuess());
 }
 start();
