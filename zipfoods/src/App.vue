60bc66d3025a791ecfbb792e00a07ad94d3b3ea5
Torre <tostaylo1@gmail.com>
2020-10-31 12:31:35 -0500

    zipfoods start with two components

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,26 @@
+
+
+
+<template>
+  <div class="app" id="app">
+    <img id="logo" alt="Vue logo" src="./assets/images/zipfoods-logo.png" />
+    <p>
+      ZipFoods is your one-stop-shop for convenient online grocery shopping in
+      the greater Boston area.
+    </p>
+    <show-products></show-products>
+  </div>
+</template>
+
+<script>
+import ShowProducts from "./components/ShowProducts.vue";
+export default {
+  name: "App",
+  components: { "show-products": ShowProducts },
+};
+</script>
+
+
+<style lang='scss'>
+@import "@/assets/scss/zipfoods.scss";
+</style>
---
+++ zipfoods/src/components/ShowProduct.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "ShowProduct",
+  props: ["product"],
+};
+</script>
+
+
+<template>
+  <div class="product">
+    <div class="product-name">{{ product.name }}</div>
+    <img
+      class="product-thumb"
+      :src="require('@/assets/images/products/1.jpg')"
+    />
+    <p class="product-description">{{ product.description }}</p>
+    <div class="product-price">${{ product.price }}</div>
+  </div>
+</template>
