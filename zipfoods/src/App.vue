e4cae3752c775047ce6599e1f70e30a0b4cdf596
Torre <tostaylo1@gmail.com>
2020-11-01 15:23:07 -0600

    products file.  Pre Vue Router commit

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -8,15 +8,24 @@
       ZipFoods is your one-stop-shop for convenient online grocery shopping in
       the greater Boston area.
     </p>
-    <show-products></show-products>
+    <show-featured :products="products" :category="'snacks'"></show-featured>
+    <show-products :products="products"></show-products>
   </div>
 </template>
 
 <script>
 import ShowProducts from "./components/ShowProducts.vue";
+import ShowFeatured from "./components/ShowFeatured.vue";
+import { products } from "@/products.js";
+
 export default {
   name: "App",
-  components: { "show-products": ShowProducts },
+  components: { "show-products": ShowProducts, "show-featured": ShowFeatured },
+  data() {
+    return {
+      products,
+    };
+  },
 };
 </script>
 
---
+++ zipfoods/src/components/ShowFeatured.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="featured">
+    <h2>Featured Products</h2>
+    <ul class="cleanList">
+      <li v-for="product in featuredProducts" :key="product.id">
+        {{ product.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: ["category", "products"],
+  computed: {
+    featuredProducts() {
+      return this.products.filter((product) => {
+        return product.categories.includes(this.category);
+      }, this.category);
+    },
+  },
+};
+</script>
+
