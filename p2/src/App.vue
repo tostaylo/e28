c5eef4c4739d71eff77185478045d832f85f3df1
Torre <tostaylo1@gmail.com>
2020-11-06 21:42:42 -0600

    p2 project setup

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view :stats="stats" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return { stats: null };
+  },
+
+  mounted() {
+    fetch("/trace_results.json")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.stats = data;
+      });
   }
-}
+};
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #98a3ad;
+  background: hsl(200, 7%, 8%);
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  min-height: 100vh;
+  width: 100vw;
 }
 </style>
---
+++ p2/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    {{ stats }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Options({
+  props: ["stats"],
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {}
+</script>
